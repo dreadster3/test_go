d0c31069f16af4e181b600c1e79786831da3508c
dreadster3 <afonso.antunes@live.com.pt>
2024-03-08 17:35:55 +0000

    Add log

--- version.go
+++ version.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 var Version = "dev"
 
 func init() {
+	info, ok := debug.ReadBuildInfo()
 
+	if ok {
+		fmt.Println("Build Info", info.Main.Path, info.Main.Version)
+	}
 }
