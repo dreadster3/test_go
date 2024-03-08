ac16a9a6f0371c3fa996af6b1ca991646878226e
dreadster3 <afonso.antunes@live.com.pt>
2024-03-08 17:38:05 +0000

    Set version

--- version.go
+++ version.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"runtime/debug"
 )
 
 var Version = "dev"
 
 func init() {
-	info, ok := debug.ReadBuildInfo()
+	if Version == "dev" {
+		info, ok := debug.ReadBuildInfo()
 
-	if ok {
-		fmt.Println("Build Info", info.Main.Path, info.Main.Version)
+		if ok {
+			Version = info.Main.Version
+		}
 	}
 }
